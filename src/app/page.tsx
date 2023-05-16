5b31d87bbe969f1b75580ecc99a8cf6bca5e5010
h662Olbm <h662@olbm.app>
2023-05-17 07:30:05 +0900

    2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ const revealCheck = [
   { id: 2, format: "png", isRevealed: false },
   { id: 3, format: "png", isRevealed: true },
   { id: 4, format: "png", isRevealed: false },
-  { id: 5, format: "png", isRevealed: false },
+  { id: 5, format: "png", isRevealed: true },
   { id: 6, format: "png", isRevealed: false },
 ];
 
